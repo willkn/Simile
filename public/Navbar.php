81cccc609fe1adc00998d8b7c7e54a89209e64d1
CJFalby <charlotte.falby03@gmail.com>
2024-02-19 10:28:34 +0000

    Edits

--- public/Navbar.php
+++ public/Navbar.php
@@ -2,12 +2,20 @@
 <html lang="en">
 
 <head>
-    <title>Similie</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="site.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
 </head>
+
+<body>
+    <header>
+        <nav style="height: 70px; background-color:grey">
+            <div>
+                <a class="titlefont" style="color:black">Similie</a>
+            </div>
+        </nav>
+    </header>
+</body>
