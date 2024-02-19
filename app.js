13af3d8ec92464283bb994ad086eea643d087efe
CJFalby <charlotte.falby03@gmail.com>
2024-02-19 09:32:25 +0000

    Start page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { spawn } = require('child_process');
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
 
 const fs = require('fs');
 const { DOMParser } = require('xmldom');
@@ -9,103 +10,92 @@ const { DOMParser } = require('xmldom');
 const content = 'Some content!';
 
 function xmlToGraph(pathToGraph) {
-  // Read from our collection of graphs
-  const xmlString = fs.readFileSync(pathToGraph, 'utf8');
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+    // Read from our collection of graphs
+    const xmlString = fs.readFileSync(pathToGraph, 'utf8');
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
 
-  // Find all <mxCell> elements within the <mxGraphModel> element
-  const mxGraphModel = xmlDoc.getElementsByTagName('mxGraphModel')[0];
-  const mxCellNodes = Array.from(mxGraphModel.getElementsByTagName('mxCell'));
+    // Find all <mxCell> elements within the <mxGraphModel> element
+    const mxGraphModel = xmlDoc.getElementsByTagName('mxGraphModel')[0];
+    const mxCellNodes = Array.from(mxGraphModel.getElementsByTagName('mxCell'));
 
-  // Create a dictionary to store the connections
-  const connections = {};
+    // Create a dictionary to store the connections
+    const connections = {};
 
-  // Iterate through each <mxCell> and identify its source and target
-  for (const mxCellNode of mxCellNodes) {
-    const sourceId = mxCellNode.getAttribute('source');
-    const targetId = mxCellNode.getAttribute('target');
-    const valueId = mxCellNode.getAttribute('value');
+    // Iterate through each <mxCell> and identify its source and target
+    for (const mxCellNode of mxCellNodes) {
+        const sourceId = mxCellNode.getAttribute('source');
+        const targetId = mxCellNode.getAttribute('target');
+        const valueId = mxCellNode.getAttribute('value');
 
-    // Check if the mxCell represents an edge (has both source and target)
-    if (sourceId && targetId) {
-      if (!connections[sourceId]) {
-        connections[sourceId] = [];
-      }
-      const temp = [targetId, valueId] 
-      connections[sourceId].push(temp);
+        // Check if the mxCell represents an edge (has both source and target)
+        if (sourceId && targetId) {
+            if (!connections[sourceId]) {
+                connections[sourceId] = [];
+            }
+            const temp = [targetId, valueId]
+            connections[sourceId].push(temp);
+        }
     }
-  }
-  // Print the connections
-  return connections
+    // Print the connections
+    return connections
 }
 
-function extractNodes(pathToGraph)
-{
-  const xmlString = fs.readFileSync(pathToGraph, 'utf8');
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+function extractNodes(pathToGraph) {
+    const xmlString = fs.readFileSync(pathToGraph, 'utf8');
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
 
-  // Find all <mxCell> elements within the <mxGraphModel> element
-  const mxGraphModel = xmlDoc.getElementsByTagName('mxGraphModel')[0];
-  const mxCellNodes = Array.from(mxGraphModel.getElementsByTagName('mxCell'));
+    // Find all <mxCell> elements within the <mxGraphModel> element
+    const mxGraphModel = xmlDoc.getElementsByTagName('mxGraphModel')[0];
+    const mxCellNodes = Array.from(mxGraphModel.getElementsByTagName('mxCell'));
 
-  let nodes = {};
+    let nodes = {};
 
-  for (const mxCellNode of mxCellNodes) 
-  {
-    const sourceId = mxCellNode.getAttribute('source');
-    const targetId = mxCellNode.getAttribute('target');
+    for (const mxCellNode of mxCellNodes) {
+        const sourceId = mxCellNode.getAttribute('source');
+        const targetId = mxCellNode.getAttribute('target');
 
-    const value = mxCellNode.getAttribute('value');
-    //check if it's a node
-    if (!sourceId && !targetId && value)
-    {
-      const id = mxCellNode.getAttribute('id');
+        const value = mxCellNode.getAttribute('value');
+        //check if it's a node
+        if (!sourceId && !targetId && value) {
+            const id = mxCellNode.getAttribute('id');
 
-      nodes[id] = removeHTMLSymbols(value);;
+            nodes[id] = removeHTMLSymbols(value);;
+        }
     }
-  }
-  return nodes;
+    return nodes;
 }
 
-function xmlToCSV3(connections, nodes)
-{
-  console.log(connections);
-  //go through every array of connection
-  let toTxt = "";
+function xmlToCSV3(connections, nodes) {
+    console.log(connections);
+    //go through every array of connection
+    let toTxt = "";
 
-  for (const [key, value] of Object.entries(connections))
-  {
-    for(const connection of value)
-    {
-      const targetId = connection[0];
-      const value = connection[1];
+    for (const [key, value] of Object.entries(connections)) {
+        for (const connection of value) {
+            const targetId = connection[0];
+            const value = connection[1];
 
-      toTxt = toTxt.concat(nodes[key] + "," + value + "," + nodes[targetId] + "\n");
+            toTxt = toTxt.concat(nodes[key] + "," + value + "," + nodes[targetId] + "\n");
+        }
+        //nodeConnections = connection.value
     }
-    //nodeConnections = connection.value
-  }
-  return toTxt;
+    return toTxt;
 }
 
-function removeHTMLSymbols(str)
-{
-  const htmlSymbols = ["&nbsp;", "&lt;", "&gt;", "&amp;", "&quot;", "<div>", "</div>", "<span>", "</span>"];
+function removeHTMLSymbols(str) {
+    const htmlSymbols = ["&nbsp;", "&lt;", "&gt;", "&amp;", "&quot;", "<div>", "</div>", "<span>", "</span>"];
 
-  for(i = 0; i < htmlSymbols.length; i++)
-  {
-    //check if you need <span> in the if condition too 
-    if(htmlSymbols[i] == "<div>")
-    {
-      str = str.replace(htmlSymbols[i], " ");
+    for (i = 0; i < htmlSymbols.length; i++) {
+        //check if you need <span> in the if condition too 
+        if (htmlSymbols[i] == "<div>") {
+            str = str.replace(htmlSymbols[i], " ");
+        } else {
+            str = str.replace(htmlSymbols[i], "");
+        }
     }
-    else
-    {
-      str = str.replace(htmlSymbols[i], "");
-    }
-  }
-  return str;
+    return str;
 }
 
 
@@ -115,18 +105,18 @@ let nodes = extractNodes('./graphs/graph.xml');
 
 let result = xmlToCSV3(listOfConnections, nodes);
 fs.writeFile('test.txt', result, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    // file written successfully
-  }
+    if (err) {
+        console.error(err);
+    } else {
+        // file written successfully
+    }
 });
 
 app.use(express.json()); // For JSON body parsing
 app.use(express.urlencoded({ extended: true })); // For URL-encoded body parsing
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.post('/cgfca', (req, res) => {
@@ -139,16 +129,16 @@ let inputQueue = [];
 
 // Endpoint for C++ application to send output
 app.post('/output', (req, res) => {
-  console.log('C++ Output:', req.body.message);
-  res.status(200).send({status: 'Received'});
+    console.log('C++ Output:', req.body.message);
+    res.status(200).send({ status: 'Received' });
 });
 
 // Endpoint for C++ application to request input
 app.get('/input', (req, res) => {
-  if (inputQueue.length > 0) {
-      const input = inputQueue.shift(); // Get the first item from the queue
-      res.status(200).send({input: input});
-  } else {
-      res.status(404).send({error: 'No input available'});
-  }
+    if (inputQueue.length > 0) {
+        const input = inputQueue.shift(); // Get the first item from the queue
+        res.status(200).send({ input: input });
+    } else {
+        res.status(404).send({ error: 'No input available' });
+    }
 });
